e80cfc00172462596b27253364fdffc5491af584
MisterFail <jimmy.djabali@epitech.eu>
2018-09-09 01:26:23 +0200

    forget

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import React from "react";
 import AppContainer from "./components/app-container";
-import Navbar from "./components/navbar";
-import Main from "./components/main";
+import BaseContainer from "./components/base/components/base-container";
+import Navbar from "./components/base/components/navbar";
+import Main from "./components/base/components/main";
+import Popup from "./components/popup";
 
 const App = () => (
   <AppContainer>
-    <Navbar />
-    <Main />
+    <BaseContainer>
+      <Navbar />
+      <Main />
+    </BaseContainer>
+    <Popup />
   </AppContainer>
 );
 
