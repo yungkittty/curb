6e4d3fb211f0ba67eda07fc4314caa4f78076aab
MisterFail <jimmy.djabali@epitech.eu>
2018-09-21 16:36:49 +0800

    Removed Link component (unused)

--- src/components/general/link/components/link-container/link-container.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-const LinkContainer = styled.a``;
-
-export default LinkContainer;
--- src/components/general/link/link.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import LinkContainer from "./components/link-container";
-
-const Link = ({ link }) => <LinkContainer href={link} />;
-
-export default Link;
