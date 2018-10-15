5a2b983d25512efd36c8b886cc956624d7b6dedb
MisterFail <jimmy.djabali@epitech.eu>
2018-10-15 23:58:26 +0800

    added sign up default

--- src/scenes/sign-up/sign-up.js
+++ src/scenes/sign-up/sign-up.js
@@ -1,3 +1,14 @@
-const SignUp = () => null;
+import React from "react";
+import PopupWindow from "../../components/popup/components/popup-window";
+import SignupContent from "./components/signup-content";
+
+const SignUp = () => (
+  <PopupWindow
+    title="Sign up"
+    button="Next"
+    buttonLink="/sign-in"
+    content={SignupContent}
+  />
+);
 
 export default SignUp;
