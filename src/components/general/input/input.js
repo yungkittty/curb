c642d41c116acc495785cdf28662a8a5111bc910
MisterFail <jimmy.djabali@epitech.eu>
2018-10-16 13:44:38 +0800

    added placeholder handling for input general component

--- src/components/general/input/input.js
+++ src/components/general/input/input.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
-const Input = styled.input`
+const Input = styled.input.attrs({
+  placeholder: props => props.placeholder
+})`
   font-size: 18px;
   padding: 18px;
   border: 0;
-  border-bottom: 1px solid #C8CCD4;
+  border-bottom: 1px solid #c8ccd4;
   display: inline-block;
   witdh: 380px;
 `;
