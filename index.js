5be13b27537c5615d8caffe9a0551c3e0829e81f
MisterFail <jimmy.djabali@epitech.eu>
2018-10-25 22:26:33 +0800

    Merge branch 'develop' into feature/11

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import { AppRegistry } from "react-native";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-// import Root from "./src/root";
-import App from "./src/app";
+import Root from "./src/root";
 import { name as appName } from "./app.json";
 
 library.add(fas);
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Root);
