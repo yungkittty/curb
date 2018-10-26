ce27931194ece0008c18eb8336722a04b4c2d30e
MisterFail <jimmy.djabali@epitech.eu>
2018-10-26 15:04:42 +0800

    Update android/app/src/main/java/com/curb/MainApplication.java

--- android/app/src/main/java/com/curb/MainApplication.java
+++ android/app/src/main/java/com/curb/MainApplication.java
@@ -23,8 +23,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new ReactNativeI18n()
+            new VectorIconsPackage()
       );
     }
 
