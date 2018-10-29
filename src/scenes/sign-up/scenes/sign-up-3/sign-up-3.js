a3bd581931776066f4f8a5c6d2a06797d141ae69
MisterFail <jimmy.djabali@epitech.eu>
2018-10-29 09:12:36 +0800

    Removed display informations at end of /sign-up

--- src/scenes/sign-up/scenes/sign-up-3/sign-up-3.js
+++ src/scenes/sign-up/scenes/sign-up-3/sign-up-3.js
@@ -3,21 +3,12 @@ import ContentContainer from "./components/content-container";
 import ContentText from "./components/content-text";
 import Text from "../../../../components/text";
 
-const Signup3 = ({ data }) => {
+const Signup3 = () => {
   return (
     <ContentContainer>
       <ContentText hm>
         <Text va h1 center b p>
           Your account has been created!
-        </Text>
-        <Text p>
-          Username : <Text i>{data.username}</Text>
-        </Text>
-        <Text p>
-          Password : <Text i>{data.pass}</Text>
-        </Text>
-        <Text p>
-          Confirm : <Text i>{data.passConfirm}</Text>
         </Text>
       </ContentText>
     </ContentContainer>
