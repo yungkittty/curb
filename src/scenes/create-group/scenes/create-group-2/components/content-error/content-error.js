ed35579d7d37fe0c9030c5c3b2108d96b888904c
MisterFail <jimmy.djabali@epitech.eu>
2019-01-01 17:05:28 +0800

    Better error font size & position

--- src/scenes/create-group/scenes/create-group-2/components/content-error/content-error.js
+++ src/scenes/create-group/scenes/create-group-2/components/content-error/content-error.js
@@ -1,12 +1,17 @@
 import styled from "styled-components";
 import Text from "../../../../../../components/text";
+import { screenWidthsMedias } from "../../../../../../configurations/screen";
 
 const ContentError = styled(Text)`
   position: absolute;
-  font-size: 16px;
+  font-size: 14px;
   color: ${({ theme }) => theme.errorColor};
   margin: auto;
-  margin-top: 42px;
+  margin-top: 44px;
+
+  ${screenWidthsMedias.medium`
+    font-size: 16px;
+  `}
 `;
 
 export default ContentError;
