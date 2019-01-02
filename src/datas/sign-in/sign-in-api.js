350e0286b95b0a28d8fa79ff601ffe1a244d608f
vm-user <quentin.woivre@epitech.eu>
2019-01-02 11:01:03 +0100

    remove double quote when useless, ...

--- src/datas/sign-in/sign-in-api.js
+++ src/datas/sign-in/sign-in-api.js
@@ -2,7 +2,7 @@ const signInApi = {
   signIn: payload =>
     fetch(`${process.env.REACT_APP_API_URL}/accounts/sign-in`, {
       method: "POST",
-      headers: { "Accept": "application/json", "Content-Type": "application/json" },
+      headers: { Accept: "application/json", "Content-Type": "application/json" },
       body: JSON.stringify(payload)
     }),
   signOut: token =>
