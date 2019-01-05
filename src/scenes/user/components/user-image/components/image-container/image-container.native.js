edad007b67f37782fa061bd3e3486b52fbee1e22
Hugzton <hugo.toussaint@epitech.eu>
2019-01-05 19:05:01 +0100

    fix image size on user scene

--- src/scenes/user/components/user-image/components/image-container/image-container.native.js
+++ src/scenes/user/components/user-image/components/image-container/image-container.native.js
@@ -1,21 +1,15 @@
 import styled from "styled-components";
 import Container from "../../../../../../components/container";
-import { screenWidthsMedias } from "../../../../../../configurations/screen";
 
 const ImageContainer = styled(Container)`
     position: relative;
     display: flex;
-    border-radius: 170px;
+    border-radius: 140px;
     overflow: hidden;
-    width: 170px;
-    height: 170px;
+    width: 140px;
+    height: 140px;
     margin-top: 70px;
 
-    ${screenWidthsMedias.large`
-    border-radius: 200px;
-    width: 200px;
-    height: 200px;
-  `};
 `;
 
 export default ImageContainer;
