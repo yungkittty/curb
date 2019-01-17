6cd0588500e68cc373d346ec5003d1959a752418
MisterFail <jimmy.djabali@epitech.eu>
2019-01-17 17:25:42 +0800

    Sign-in-api : removed enclosing object between ...payload, switch from null to undefined for signOut

--- src/datas/sign-in/sign-in-api.js
+++ src/datas/sign-in/sign-in-api.js
@@ -2,13 +2,15 @@ import axios from "axios";
 
 const signInApi = {
   signIn: payload =>
-    axios.post(`${process.env.REACT_APP_API_URL}/accounts/sign-in`, {
-      ...payload
-    }),
+    axios.post(`${process.env.REACT_APP_API_URL}/accounts/sign-in`, payload),
   signOut: token =>
-    axios.post(`${process.env.REACT_APP_API_URL}/accounts/sign-out`, null, {
-      headers: { Authorization: `Bearer ${token}` }
-    })
+    axios.post(
+      `${process.env.REACT_APP_API_URL}/accounts/sign-out`,
+      undefined,
+      {
+        headers: { Authorization: `Bearer ${token}` }
+      }
+    )
 };
 
 export default signInApi;
