409b3b2b1afbbc8a830894d2666bcf99bb15f2ce
MisterFail <jimmy.djabali@epitech.eu>
2019-01-18 18:49:51 +0800

    added payload for postGroup request

--- src/datas/groups/groups-api.js
+++ src/datas/groups/groups-api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const groupsApi = {
-  postGroup: () => axios.post(`${process.env.REACT_APP_API_URL}/groups`),
+  postGroup: payload =>
+    axios.post(`${process.env.REACT_APP_API_URL}/groups`, payload),
   getGroup: ({ id }) =>
     axios.get(`${process.env.REACT_APP_API_URL}/groups/${id}`)
 };
