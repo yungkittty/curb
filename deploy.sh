948b8caa243a983266ad41db3df42f6e18c66934
MisterFail <jimmy.djabali@epitech.eu>
2019-03-04 00:54:23 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@
 # Install modules
 npm install
 
+# Build web
+# npm run web-build
+
 # Install Java
 wget -q -O jdk.tar.gz https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
 tar -xf jdk.tar.gz
@@ -11,7 +14,7 @@ tar -xf jdk.tar.gz
 # Install Android SDK
 wget -q -O sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 mkdir sdk && mv sdk.zip sdk && cd sdk && unzip -qq sdk.zip
-yes | ./tools/bin/sdkmanager "build-tools;28.0.3" "platforms;android-28" &> /dev/null
+yes | ./tools/bin/sdkmanager "build-tools;27.0.3" "platforms;android-27" &> /dev/null
 cd ..
 
 # Install NDK
@@ -22,24 +25,23 @@ unzip -qq ndk.zip
 echo ---------------- STATUS ---------------
 
 
-# Build web
-# npm run web-build
-
-# Export Mobile variables
-ls sdk
-export ANDROID_HOME=/opt/build/repo
-
-
 # Clearup before mobile build
+df -h
 rm -rf jdk.tar.gz sdk.zip ndk.zip
+df -h
+
+# Export variables for mobile build
+ls /opt/build/repo/sdk/build-tools/27.0.3/
+export ANDROID_HOME=/opt/build/repo/sdk/build-tools/27.0.3/
+
 
 # Build native (Android)
-react-native bundle                                                 \
---platform android                                                  \
---dev false                                                         \
---entry-file index.js                                               \
---bundle-output android/app/src/main/assets/index.android.bundle    \
---assets-dest android/app/src/main/res/
+#react-native bundle                                                 \
+#--platform android                                                  \
+#--dev false                                                         \
+#--entry-file index.js                                               \
+#--bundle-output android/app/src/main/assets/index.android.bundle    \
+#--assets-dest android/app/src/main/res/
 
 cd ./android && ./gradlew assembleRelease && cd ..
 #cp ./android/app/build/outputs/apk/release/app-release.apk build/app.apk
