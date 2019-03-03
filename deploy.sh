81525c5c97dda6755bf616d35e48ad42c0a3fff0
MisterFail <jimmy.djabali@epitech.eu>
2019-03-03 22:58:32 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,16 +12,16 @@ docker info
 # npm run web-build
 
 # Install Java
-wget --continue --no-check-certificate -O jdk.zip --header "Cookie: oraclelicense=a" https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jdk-8u201-linux-x64.tar.gz
-unzip jdk.zip
+#wget --continue --no-check-certificate -O jdk.zip --header "Cookie: oraclelicense=a" https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jdk-8u201-linux-x64.tar.gz
+#unzip jdk.zip
 
 # Install Android SDK
-wget --output-file=sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
-unzip sdk.zip
-sdkmanager "platform-tools" "platforms;android-28"
+#wget --output-file=sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+#unzip sdk.zip
+#sdkmanager "platform-tools" "platforms;android-28"
 
 
-wget https://dl.google.com/android/repository/android-ndk-r19b-linux-x86_64.zip
+#wget https://dl.google.com/android/repository/android-ndk-r19b-linux-x86_64.zip
 
 
 # Build native (Android)
