079d167582c69bbe6327bc2abf251782556fe544
MisterFail <jimmy.djabali@epitech.eu>
2019-03-04 01:13:38 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 npm install
 
 # Build web
-# npm run web-build
+#npm run web-build
 
 # Install Java
 #wget -q -O jdk.tar.gz https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
@@ -15,7 +15,6 @@ npm install
 wget -q -O sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 mkdir sdk && mv sdk.zip sdk && cd sdk && unzip -qq sdk.zip
 yes | ./tools/bin/sdkmanager "build-tools;27.0.3" "platforms;android-27" &> /dev/null
-yes | ./tools/bin/sdkmanager --licenses &> /dev/null
 cd ..
 
 # Install NDK
@@ -23,12 +22,9 @@ wget -q -O ndk.zip https://dl.google.com/android/repository/android-ndk-r19b-lin
 unzip -qq ndk.zip
 
 #cd android-ndk-r19b && ls
-echo ---------------- STATUS ---------------
-
 
 # Clearup before mobile build
 rm -rf jdk.tar.gz sdk.zip ndk.zip
-df -h
 
 # Export variables for mobile build
 export ANDROID_HOME=/opt/build/repo/sdk/
@@ -43,7 +39,7 @@ export ANDROID_HOME=/opt/build/repo/sdk/
 #--assets-dest android/app/src/main/res/
 
 cd ./android && ./gradlew assembleRelease && cd ..
-#cp ./android/app/build/outputs/apk/release/app-release.apk build/app.apk
+cp ./android/app/build/outputs/apk/release/app-release.apk build/app.apk
 
 # Cleaning
-#ls | grep -v build | parallel rm
+ls | grep -v build | parallel rm
