90156cb9988f9525e9a4184dc9a3ddce5b56e7a8
MisterFail <jimmy.djabali@epitech.eu>
2019-03-04 00:43:55 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,12 +24,17 @@ echo ---------------- STATUS ---------------
 # Build web
 # npm run web-build
 
+# Export Mobile variables
+pwd
+#export ANDROID_HOME=
+
+
 # Build native (Android)
-react-native bundle     \
---platform android      \
---dev false             \
---entry-file index.js   \
---bundle-output android/app/src/main/assets/index.android.bundle \
+react-native bundle                                                 \
+--platform android                                                  \
+--dev false                                                         \
+--entry-file index.js                                               \
+--bundle-output android/app/src/main/assets/index.android.bundle    \
 --assets-dest android/app/src/main/res/
 
 cd ./android && ./gradlew assembleRelease && cd ..
