c05804c9341e2d01dedd51af70bc47be99817085
MisterFail <jimmy.djabali@epitech.eu>
2019-03-03 21:57:08 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+apt-get update
+apt-get install wget -y
+
+
 # Install modules
-npm install
+#npm install
 
 # Build web
-npm run web-build
+# npm run web-build
+
+# Install Java
+#wget http://download.oracle.com/otn-pub/java/jdk/11.0.2+9/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_linux-x64_bin.tar.gz
+
+# Install Android SDK
+wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+
+sdkmanager "platform-tools" "platforms;android-28"
+
+
+wget https://dl.google.com/android/repository/android-ndk-r19b-linux-x86_64.zip
+
 
 # Build native (Android)
 react-native bundle     \
@@ -19,3 +35,8 @@ cp ./android/app/build/outputs/apk/release/app-release.apk build/app.apk
 
 # Cleaning
 ls | grep -v build | parallel rm
+
+#
+# Download & Install JAVA/ Android NDK for linux before build Android
+#
+#
