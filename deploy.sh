3858ca798ef2cea34efc1b2b3a4d26f67b8540cf
MisterFail <jimmy.djabali@epitech.eu>
2019-03-05 11:26:33 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,5 @@ npm run web-build
 # Zip mobile 
 zip -q -x \*.git\* -x "/\node_modules/*" -x "/\build/*" -r curb.zip .
 
-#env
-
 # Send mobile
 curl -X POST -F file=@curb.zip -o build/app.apk -m 900 51.38.49.133:8997
