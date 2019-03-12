a93829e00c90e8ffbe365a306976c7534bf4bc14
MisterFail <jimmy.djabali@epitech.eu>
2019-03-12 16:12:20 +0800

    Test with Travis CI

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 
 
-# Initializing variables
-export HEAD_BRANCH=$(echo $HEAD | tr '/' '-')
+# Print executed commands in the terminal
+set -xe
 
-# Install modules
-npm install
+# Initializing variables
+export HEAD_BRANCH=$(echo ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH} | tr '/' '-')
+
+# Increasing INODES
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
 # Build web
 npm run web-build
 
-# Zip mobile
-zip -q -x \*.git\* -x "/\node_modules/*" -x "/\build/*" -r curb.zip .
+# Link RN
+react-native link
 
-# Send mobile
-curl -s -X POST -F file=@curb.zip -o build/static/curb-$HEAD_BRANCH.apk -m 780 $BUILD_SERVER
+# Build mobile
+react-native bundle                                                 \
+--platform android                                                  \
+--dev false                                                         \
+--entry-file index.js                                               \
+--bundle-output android/app/src/main/assets/index.android.bundle    \
+--assets-dest android/app/src/main/res/
+chmod +x android/gradlew
+cd android && ./gradlew -q -x bundleReleaseJsAndAssets assembleRelease && cd ..
+
+# Copy Android APK to web build
+cp "android/app/build/outputs/apk/release/app-release.apk" "build/static/${HEAD_BRANCH}.apk"
+
+# Send APK to TestFairy
+chmod +x testfairy-uploader.sh && ./testfairy-uploader.sh "build/static/${HEAD_BRANCH}.apk" "$HEAD_BRANCH"
+
+# Deploy to Netlify
+NETLIFY_OUTPUT=$(netlify deploy $([[ $HEAD_BRANCH == "develop" ]] && echo "--prod"); echo x)
+if [[ $HEAD_BRANCH != "develop" ]]; then
+    DEPLOY_PREVIEW_URL="https://$( echo "${NETLIFY_OUTPUT%x}" | grep -o 'Live Draft Url:.*' | cut -f3- -d/ )"
+
+    curl -q -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    -d " { \"body\": \"Preview URL:\n$DEPLOY_PREVIEW_URL\"}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+fi;
