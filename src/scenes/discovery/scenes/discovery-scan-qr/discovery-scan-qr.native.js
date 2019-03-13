088491e1712296458166c048bfd9500b46d98abc
Quentin Woivré <quentin.woivre@epitech.eu>
2019-03-13 19:59:19 +0800

    scan-qr-corner, ...

--- src/scenes/discovery/scenes/discovery-scan-qr/discovery-scan-qr.native.js
+++ src/scenes/discovery/scenes/discovery-scan-qr/discovery-scan-qr.native.js
@@ -2,8 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import { TouchableWithoutFeedback } from "react-native";
 import { RNCamera } from "react-native-camera";
-import ScanQrCamera from "./components/scan-qr-camera";
+import { withTheme } from "styled-components";
 import Container from "../../../../components/container";
+import Icon from "../../../../components/icon";
+import ScanQrCamera from "./components/scan-qr-camera";
+import ScanQrCorner from "./components/scan-qr-corner";
 import { windowDimensions } from "../../../../configurations/window";
 
 class DiscoveryScanQr extends React.Component {
@@ -21,11 +24,14 @@ class DiscoveryScanQr extends React.Component {
   }
 
   render() {
-    const { hideAppModal } = this.props;
+    const { hideAppModal, theme } = this.props;
     const { isFlashModeOn } = this.state;
     const { FlashMode } = RNCamera.Constants;
     const modalWidth = windowDimensions.width;
     const modalHeight = windowDimensions.height - 75;
+    const modalWidthOffset = (modalWidth - 250) / 2;
+    const modalHeightOffset = (modalHeight - 250) / 2;
+    const { primaryColor, secondaryColor } = theme;
     return (
       <ScanQrCamera
         style={{ flex: 1 }}
@@ -35,21 +41,65 @@ class DiscoveryScanQr extends React.Component {
       >
         {() => (
           <TouchableWithoutFeedback style={{ flex: 1 }} onPress={this.toggleFlashMode}>
-            <Container
-              style={{
-                position: "absolute",
-                left: -modalWidth / 2 + (modalWidth - 200) / 2,
-                top: -modalHeight / 2 + (modalHeight - 200) / 2,
-                right: -modalWidth / 2 + (modalWidth - 200) / 2,
-                bottom: -modalHeight / 2 + (modalHeight - 200) / 2,
-                borderLeftWidth: modalWidth / 2,
-                borderTopWidth: modalHeight / 2,
-                borderRightWidth: modalWidth / 2,
-                borderBottomWidth: modalHeight / 2,
-                borderColor: "rgba(0, 0, 0, 0.25)",
-                backgroundColor: "transparent"
-              }}
-            />
+            <Container style={{ flex: 1 }}>
+              <ScanQrCorner
+                style={{
+                  left: modalWidthOffset,
+                  top: modalHeightOffset,
+                  borderLeftWidth: 2.5,
+                  borderTopWidth: 2.5
+                }}
+              />
+              <ScanQrCorner
+                style={{
+                  top: modalHeightOffset,
+                  right: modalWidthOffset,
+                  borderTopWidth: 2.5,
+                  borderRightWidth: 2.5
+                }}
+              />
+              <Container
+                style={{
+                  position: "absolute",
+                  left: -modalWidth / 2 + modalWidthOffset,
+                  top: -modalHeight / 2 + modalHeightOffset,
+                  right: -modalWidth / 2 + modalWidthOffset,
+                  bottom: -modalHeight / 2 + modalHeightOffset,
+                  borderLeftWidth: modalWidth / 2,
+                  borderTopWidth: modalHeight / 2,
+                  borderRightWidth: modalWidth / 2,
+                  borderBottomWidth: modalHeight / 2,
+                  borderColor: "rgba(0, 0, 0, 0.25)",
+                  backgroundColor: "transparent"
+                }}
+              />
+              <Icon
+                icon="bolt"
+                size="medium"
+                color={isFlashModeOn ? primaryColor : secondaryColor}
+                style={{
+                  position: "absolute",
+                  left: modalWidth / 2 - 10,
+                  bottom: modalHeightOffset / 2 - 10
+                }}
+              />
+              <ScanQrCorner
+                style={{
+                  right: modalWidthOffset,
+                  bottom: modalHeightOffset,
+                  borderRightWidth: 2.5,
+                  borderBottomWidth: 2.5
+                }}
+              />
+              <ScanQrCorner
+                style={{
+                  bottom: modalHeightOffset,
+                  left: modalWidthOffset,
+                  borderBottomWidth: 2.5,
+                  borderLeftWidth: 2.5
+                }}
+              />
+            </Container>
           </TouchableWithoutFeedback>
         )}
       </ScanQrCamera>
@@ -59,7 +109,9 @@ class DiscoveryScanQr extends React.Component {
 
 DiscoveryScanQr.propTypes = {
   setAppModalHeaderText: PropTypes.func.isRequired,
-  hideAppModal: PropTypes.func.isRequired
+  hideAppModal: PropTypes.func.isRequired,
+  // eslint-disable-next-line
+  theme: PropTypes.object.isRequired
 };
 
-export default DiscoveryScanQr;
+export default withTheme(DiscoveryScanQr);
