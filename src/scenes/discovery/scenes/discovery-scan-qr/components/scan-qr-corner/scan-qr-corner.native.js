b40bff9e4510c02dc42561ac67feb49c3f8c3885
Quentin Woivré <quentin.woivre@epitech.eu>
2019-03-13 22:29:01 +0800

    scan-qr-camera-corner to scan-qr-corner, ...

--- src/scenes/discovery/scenes/discovery-scan-qr/components/scan-qr-corner/scan-qr-corner.native.js
+++ src/scenes/discovery/scenes/discovery-scan-qr/components/scan-qr-corner/scan-qr-corner.native.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 import Container from "../../../../../../components/container";
 
-const ScanQrCameraCorner = styled(Container)`
+const ScanQrCorner = styled(Container)`
   position: absolute;
   width: 20px;
   height: 20px;
   border-color: ${props => props.theme.primaryColor};
 `;
 
-export default ScanQrCameraCorner;
+export default ScanQrCorner;
