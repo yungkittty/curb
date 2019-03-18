09e7c9ce792bbb77eb931cc678c30ccbc5ffd2f4
MisterFail <jimmy.djabali@epitech.eu>
2019-03-18 12:28:51 +0800

    HEAD_BRANCH -> CURB_VERSION

--- deploy-netlify.sh
+++ deploy-netlify.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Deploy to Netlify
-NETLIFY_OUTPUT=$(netlify deploy $([[ "$HEAD_BRANCH" == "develop" ]] && echo "--prod"); echo x)
-if [[ "$HEAD_BRANCH" != "develop" ]]; then
+NETLIFY_OUTPUT=$(netlify deploy $([[ "$CURB_VERSION" == "develop" ]] && echo "--prod"); echo x)
+if [[ "$CURB_VERSION" != "develop" ]]; then
     DEPLOY_PREVIEW_URL="https://$( echo "${NETLIFY_OUTPUT%x}" | grep -o 'Live Draft Url:.*' | cut -f3- -d/ )"
 
     curl -q -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
