88ea2f666737edb0cc905133a8f9c2e1578c7b50
Quentin Woivré <quentin.woivre@epitech.eu>
2019-03-18 18:18:49 +0800

    react-native, ...

--- android/app/src/main/java/com/curb/MainApplication.java
+++ android/app/src/main/java/com/curb/MainApplication.java
@@ -3,8 +3,8 @@ package com.curb;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNCameraPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new RNCameraPackage()
       );
     }
 
