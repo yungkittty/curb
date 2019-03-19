5ab858de86ae84867b1bd9b557cb0a30d9ceed99
Jimmy <jimmy.djabali@epitech.eu>
2019-03-19 08:24:51 +0100

    qsd

--- ios/ci-script.sh
+++ ios/ci-script.sh
@@ -2,9 +2,9 @@
 
 
 # Decrypting
-openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in certs/developer-cert.cer.enc -d -a
-openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in certs/development-key.p12.enc -d -a
-openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in certs/curb.mobileprovision.enc -d -a
+openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in certs/developer-cert.cer.enc -d -a -out certs/developer-cert.cer
+openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in certs/development-key.p12.enc -d -a -out certs/development-key.p12
+openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in certs/curb.mobileprovision.enc -d -a -out certs/curb.mobileprovision
 
 # Create keychain
 security create-keychain -p "$CUSTOM_KEYCHAIN_PASSWORD" ios-build.keychain
@@ -16,6 +16,8 @@ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 security import certs/AppleWWDRCA.cer -k ios-build.keychain -A
 security import certs/developer-cert.cer -k ios-build.keychain -A
 security import certs/development-key.p12 -k ios-build.keychain -P "$DEV_KEY_PASSWORD" -A
+
+# Keychain fix
 security set-key-partition-list -S apple-tool:,apple: -s -k "$CUSTOM_KEYCHAIN_PASSWORD" ios-build.keychain > /dev/null
 
 # Import Provisioning Profile
