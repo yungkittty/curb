7974708007c859cacb1e207a487390475cc7ccfc
Jimmy <vm-user@vm-users-iMac.local>
2019-03-19 07:21:08 +0100

    test

--- ios/ci-script.sh
+++ ios/ci-script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Decrypting
-openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in certs/ios_development.cer.enc -d -a -out certs/ios_development.cer
-openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in certs/2020_Curb.mobileprovision.enc -d -a -out certs/2020_Curb.mobileprovision
+openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in certs/developer-cert.cer.enc -d -a -out certs/developer-cert.cer
+openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in certs/development-key.p12.enc -d -a -out certs/development-key.p12
+openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in certs/curb.mobileprovision.enc -d -a -out certs/curb.mobileprovision
 
 # Create keychain
 security create-keychain -p "$CUSTOM_KEYCHAIN_PASSWORD" ios-build.keychain
@@ -12,7 +13,8 @@ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 # Import Certificates to keychain
 security import certs/AppleWWDRCA.cer -k ios-build.keychain -A
-security import certs/ios_development.cer -k ios-build.keychain -A
+security import certs/developer-cert.cer -k ios-build.keychain -A
+security import certs/development-key.p12 -k ios-build.keychain -P "$DEV_KEY_PASSWORD" -A
 #security set-key-partition-list -S apple-tool:,apple: -s -k $SECURITY_PASSWORD ios-build.keychain > /dev/null
 
 # Import Provisioning Profile
