ef7528dbc9f2e2d92b8bb0cc1490782e19515cfa
MisterFail <jimmy.djabali@epitech.eu>
2019-03-20 17:57:10 +0800

    Update app-container.js

--- src/components/app-container/app-container.js
+++ src/components/app-container/app-container.js
@@ -1,12 +1,7 @@
 import styled from "styled-components";
 import Container from "../container";
 
-<<<<<<< HEAD
-const AppContainer = styled(Container)`
-  display: flex;
-=======
 const AppContainer = styled(Container).attrs({ id: "app-container" })`
->>>>>>> dfbab837c3013ef21759b82a57f5605c1ea4f679
   position: absolute;
   width: 100%;
   height: 100%;
