898e0e64a24dd20dd3ba48a28dfc97ae7304b877
MisterFail <jimmy.djabali@epitech.eu>
2019-03-20 13:53:53 +0800

    No Testfariy on development environment

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import TestFairy from "react-native-testfairy";
 import Root from "./src/root";
 import { name as appName } from "./app.json";
 
-TestFairy.begin("SDK-uR7nl8VP");
+if (process.env.NODE_ENV !== "development") TestFairy.begin("SDK-uR7nl8VP");
 
 AppRegistry.registerComponent(appName, () => Root);
