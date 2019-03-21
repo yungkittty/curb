6066c3f540cd3cfbd0aaa3e9a13a293fd46cc7a1
Quentin Woivré <quentin.woivre@epitech.eu>
2019-03-21 12:26:46 +0800

    button-container, button, ...

--- src/components/app-download/components/download-button/download-button.js
+++ src/components/app-download/components/download-button/download-button.js
@@ -2,14 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withTheme } from "styled-components";
 import { isAndroid } from "react-device-detect";
-import ButtonContainer from "./components/button-container";
+import ButtonContainer from "../../../button-container";
 import ButtonIcon from "./components/button-icon";
 import Text from "../../../text";
 
 const DownloadButton = ({ t, theme }) => (
   <ButtonContainer
-    style={{ textDecoration: "initial" }}
-    href={`https://tsfr.io/curb-${isAndroid ? "android" : "ios"}`}
+    onClick={`https://tsfr.io/curb-${isAndroid ? "android" : "ios"}`}
+    style={{
+      position: "relative",
+      width: 220,
+      height: 50,
+      borderRadius: 12,
+      backgroundColor: "#56ccf2"
+    }}
   >
     <React.Fragment>
       <ButtonIcon
