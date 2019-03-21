3566683669edc503d91fa3f7cef6c7f7401d5157
Quentin Woivré <quentin.woivre@epitech.eu>
2019-03-21 18:14:49 +0800

    button, ...

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -5,18 +5,18 @@ import styled from "styled-components";
 
 const Button = styled(({ children, onClick, ...others }) =>
   // eslint-disable-next-line
-  typeof onClick === "function" ? (
-    <button {...others} type="button" onClick={onClick}>
-      {React.Children.only(children)}
-    </button>
-  ) : typeof onClick === "string" && onClick.charAt(0) !== "/" ? (
+  typeof onClick === "string" && onClick.charAt(0) !== "/" ? (
     <a {...others} href={onClick}>
       {React.Children.only(children)}
     </a>
-  ) : (
+  ) : typeof onClick === "string" || onClick === "object" ? (
     <Link {...others} to={onClick}>
       {React.Children.only(children)}
     </Link>
+  ) : (
+    <button {...others} type="button" onClick={onClick}>
+      {React.Children.only(children)}
+    </button>
   )
 )`
   padding: 0px;
