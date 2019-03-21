9578c53f2cfd41c44e4696475647f2bd7078eded
MisterFail <jimmy.djabali@epitech.eu>
2019-03-21 14:31:33 +0800

    fixs

--- index.production.js
+++ index.production.js
@@ -1,5 +1,8 @@
 import { AppRegistry } from "react-native";
+import TestFairy from "react-native-testfairy";
 import Root from "./src/root";
 import { name as appName } from "./app.json";
+
+TestFairy.begin("SDK-uR7nl8VP");
 
 AppRegistry.registerComponent(appName, () => Root);
