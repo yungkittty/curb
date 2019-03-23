908e63e713f930bc18c6842c2c7056b9514b1a50
MisterFail <jimmy.djabali@epitech.eu>
2019-03-23 14:45:42 +0800

    cache + android authorization

--- src/components/image/image.native.js
+++ src/components/image/image.native.js
@@ -3,7 +3,15 @@ import { Image as ReactNativeImage } from "react-native";
 import PropTypes from "prop-types";
 
 const Image = ({ src: uri, ...others }) => (
-  <ReactNativeImage {...others} source={{ uri }} />
+  <ReactNativeImage
+    {...others}
+    source={{
+      uri,
+      headers: {
+        Pragma: "no-cache"
+      }
+    }}
+  />
 );
 
 Image.propTypes = {
