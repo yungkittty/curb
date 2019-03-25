a1212f05535271c1db8c3e02adba63d680dcfc46
MisterFail <jimmy.djabali@epitech.eu>
2019-03-25 21:42:28 +0800

    Merge branch 'develop' into features/add-settings-21

--- src/components/container-scroll/components/scroll-container-content/scroll-container-content.js
+++ src/components/container-scroll/components/scroll-container-content/scroll-container-content.js
@@ -6,15 +6,8 @@ const ScrollContainerContent = styled(Container)`
   display: flex;
   flex: 1;
   flex-direction: ${props => (props.horizontal ? "row" : "column")};
-<<<<<<< HEAD
-  width: ${props =>
-    !props.showsVerticalScrollIndicator ? "calc(100% + 30px)" : "100%"};
-  height: ${props =>
-    !props.showsHorizontalScrollIndicator ? "calc(100% + 30px)" : "100%"};
-=======
   width: ${props => (!props.showsVerticalScrollIndicator ? "calc(100% + 30px)" : "100%")};
   height: ${props => (!props.showsHorizontalScrollIndicator ? "calc(100% + 30px)" : "100%")};
->>>>>>> develop
   overflow-y: ${props => (!props.horizontal ? "auto" : "hidden")};
   overflow-x: ${props => (props.horizontal ? "auto" : "hidden")};
 `;
