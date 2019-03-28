b2f2525314e99d0aa4d921d77a5e7538e80f1923
Quentin Woivré <quentin.woivre@epitech.eu>
2019-03-28 17:06:51 +0800

    certificates, ...

--- .travis/build-ios.sh
+++ .travis/build-ios.sh
@@ -5,7 +5,7 @@ set -e
 
 # ...
 openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in curb-developer.certificate.enc -d -a -out curb-developer.certificate
-openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in curb-development-key.certificate.enc -d -a -out curb-development-key.certificate
+openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in curb-development-key.certificate.p12.enc -d -a -out curb-development-key.certificate.p12
 openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in curb.mobileprovision.enc -d -a -out curb.mobileprovision
 
 # ...
@@ -17,7 +17,7 @@ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 # ...
 security import curb-apple.certificate -k ios-build.keychain -A
 security import curb-developer.certificate -k ios-build.keychain -A
-security import curb-development-key.certificate -k ios-build.keychain -P "$DEV_KEY_PASSWORD" -A
+security import curb-development-key.certificate.p12 -k ios-build.keychain -P "$DEV_KEY_PASSWORD" -A
 
 # ...
 security set-key-partition-list -S apple-tool:,apple: -s -k "$CUSTOM_KEYCHAIN_PASSWORD" ios-build.keychain > /dev/null
