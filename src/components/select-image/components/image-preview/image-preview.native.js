13ade5b5531286333af1fa84aac5645b8ff3435d
MisterFail <jimmy.djabali@epitech.eu>
2019-03-28 18:50:28 +0800

    Fixed border on ImagePreview

--- src/components/select-image/components/image-preview/image-preview.native.js
+++ src/components/select-image/components/image-preview/image-preview.native.js
@@ -1,9 +1,21 @@
+import React from "react";
 import styled from "styled-components";
 import Image from "../../../image";
+import Container from "../../../container";
 
-const ImagePreview = styled(Image).attrs(() => ({
-  resizeMode: "cover"
-}))`
+const ImagePreview = styled(({ children, style, src, ...others }) => (
+  // eslint-disable-next-line
+  <Container style={style}>
+    {src && (
+      <Image
+        {...others}
+        style={{ width: style[0].width, height: style[0].height }}
+        src={src}
+        resizeMode="cover"
+      />
+    )}
+  </Container>
+))`
   position: absolute;
   top: 0px;
   border-radius: 100px;
