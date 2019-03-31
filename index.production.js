00a35a14f959f3c3a07278791f0537bdef53c411
Quentin Woivré <quentin.woivre@epitech.eu>
2019-03-31 12:35:03 +0800

    package.json, ...

--- index.production.js
+++ index.production.js
@@ -1,10 +1,8 @@
 import { AppRegistry } from "react-native";
 import TestFairy from "react-native-testfairy";
-// import Root from "./src/root";
+import Root from "./src/root";
 import { name as appName } from "./app.json";
 
 TestFairy.begin(process.env.TESTFAIRY_SDK_KEY);
-
-const Root = () => null;
 
 AppRegistry.registerComponent(appName, () => Root);
