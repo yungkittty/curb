4e8fdc35777e62c8f33a0a88f0771c0e101c3eaf
MisterFail <jimmy.djabali@epitech.eu>
2019-03-31 22:43:04 +0800

    Delete app-modal-footer.js

--- src/components/app-modal/components/app-modal-footer/app-modal-footer.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import FooterButtonText from "./components/footer-button-text";
-
-const AppModalFooter = props => <FooterButtonText {...props} />;
-
-export default AppModalFooter;
