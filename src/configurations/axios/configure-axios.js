287b4c947927525b269d61a904297a318edad1fd
Quentin Woivré <quentin.woivre@epitech.eu>
2019-04-03 22:32:09 +0800

    axios, ...

--- src/configurations/axios/configure-axios.js
+++ src/configurations/axios/configure-axios.js
@@ -1,7 +1,18 @@
+import _ from "lodash";
 import axios from "axios";
+import { signInActions } from "../../datas/sign-in";
 
-const configureAxios = () => {
+// https://github.com/yungkittty/curb-api/tree/documentation-api-13
+
+const configureAxios = ({ dispatch }) => {
   axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+  axios.interceptors.response.use(response => {
+    const { status, data: { code } } = response;
+    if ((status === 400 || status === 403) && _.includes(code, "TOKEN")) {
+      dispatch(signInActions.signOutSuccess());
+    }
+    return response;
+  });
 };
 
 export default configureAxios;
--- src/root.js
+++ src/root.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import App from "./app";
-import configureAxios from "./configurations/axios";
 import configureI18n from "./configurations/internationalisation";
 import configureStore from "./configurations/store";
+import configureAxios from "./configurations/axios";
 import configureTheme from "./configurations/theme";
 
-configureAxios();
 configureI18n();
 const store = configureStore();
+configureAxios(store);
 const theme = configureTheme();
 
 const Root = () => (
