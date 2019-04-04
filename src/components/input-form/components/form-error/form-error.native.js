e030d0e57f99ffda180ebde152b3c0553cb0a3f7
MisterFail <jimmy.djabali@epitech.eu>
2019-04-04 12:44:23 +0800

    Update form-error.native.js

--- src/components/input-form/components/form-error/form-error.native.js
+++ src/components/input-form/components/form-error/form-error.native.js
@@ -5,7 +5,7 @@ import Text from "../../../text";
 const FormError = styled(Text)`
   position: absolute;
   left: 0px;
-  top: ${Platform.OS === "android" ? "62" : "56"}px
+  top: ${Platform.OS === "android" ? "62" : "54"}px
   color: ${props => props.theme.errorColor};
 `;
 
