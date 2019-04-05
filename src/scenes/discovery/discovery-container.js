aba44438cc29d4974aae00aa967e7ef40f306f7d
MisterFail <jimmy.djabali@epitech.eu>
2019-04-05 16:17:17 +0800

    Update discovery-container.js

--- src/scenes/discovery/discovery-container.js
+++ src/scenes/discovery/discovery-container.js
@@ -6,7 +6,20 @@ import { discoveryActions, discoverySelectors } from "../../datas/discovery";
 
 class DiscoveryContainer extends React.Component {
   componentDidMount() {
-    const { getDiscovery } = this.props;
+    const {
+      getDiscovery,
+      location: { search }
+    } = this.props;
+    if (search) {
+      const validateEmail = "?validateEmail=";
+      const index = search.lastIndexOf(validateEmail);
+      if (index !== -1) {
+        const token = search.substring(index + validateEmail.length);
+        console.log(token);
+      }
+
+      // Call validate email
+    }
     getDiscovery({ count: 25 });
   }
 
@@ -26,7 +39,10 @@ const mapDispatchToProps = dispatch => ({
     dispatch(discoveryActions.getDiscoveryRequest(payload))
 });
 
-DiscoveryContainer.propTypes = { getDiscovery: PropTypes.func.isRequired };
+DiscoveryContainer.propTypes = {
+  getDiscovery: PropTypes.func.isRequired,
+  location: PropTypes.shape({ search: PropTypes.string.isRequired }).isRequired
+};
 
 export default connect(
   mapStateToProps,
