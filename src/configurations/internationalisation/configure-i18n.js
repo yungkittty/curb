da29654f1dbdbf0dee8a9a8b33ba846b07ddf075
Quentin Woivré <quentin.woivre@epitech.eu>
2019-04-10 18:54:38 +0800

    translations, ...

--- src/configurations/internationalisation/configure-i18n.js
+++ src/configurations/internationalisation/configure-i18n.js
@@ -1,7 +1,7 @@
 import i18next from "i18next";
 import { initReactI18next as i18nextInit } from "react-i18next";
 import i18nextLanguageDetector from "./i18next-language-detector";
-import i18nextTranslations from "./i18next-translations";
+import translations from "./translations";
 import { currentSettingsSelectors } from "../../datas/current-settings";
 
 const configureI18n = ({ getState }) => {
@@ -13,7 +13,7 @@ const configureI18n = ({ getState }) => {
     .use(i18nextInit)
     .use(i18nextLanguageDetector)
     .init({
-      resources: i18nextTranslations,
+      resources: translations,
       ...(currentSettingsLanguage ? { lng: currentSettingsLanguage } : {}),
       fallbackLng: "en",
       interpolation: { escapeValue: false }
