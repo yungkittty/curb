34b17fe121f1b9730f645455ff9694251bc20650
MisterFail <jimmy.djabali@epitech.eu>
2019-04-14 22:53:32 +0800

    Update users-reducer.js

--- src/datas/users/users-reducer.js
+++ src/datas/users/users-reducer.js
@@ -23,7 +23,6 @@ const byId = (state = {}, action) => {
         }
       };
     case usersActionsTypes.GET_USER_FAILURE:
-      return state;
       return {
         ...state,
         [action.payload.config.data.id]: {
