e9c9eccc28a02d6bcdac8b3b383068b308d9b390
MisterFail <jimmy.djabali@epitech.eu>
2019-04-16 00:09:38 +0800

    Fix doubleColor

--- src/components/app-modal-scene-list-item/components/item-preview/components/preview-description/preview-description.native.js
+++ src/components/app-modal-scene-list-item/components/item-preview/components/preview-description/preview-description.native.js
@@ -3,7 +3,7 @@ import Text from "../../../../../text";
 
 const PreviewDescription = styled(Text).attrs(() => ({ type: "h6" }))`
   color: ${({ theme, disabled }) => (!disabled ? theme.fontVariantColor : theme.secondaryVariantColor)};
-  margin-top: 4px;
+  margin-top: 6px;
 `;
 
 export default PreviewDescription;
