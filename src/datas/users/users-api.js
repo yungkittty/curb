6ff7942afae0b984230567c972c71f1235cc0651
MisterFail <jimmy.djabali@epitech.eu>
2019-04-17 18:04:56 +0800

    fixed patch user

--- src/datas/users/users-api.js
+++ src/datas/users/users-api.js
@@ -2,10 +2,7 @@ import axios from "axios";
 
 const usersApi = {
   getUser: ({ id }) => axios.get(`/users/${id}`, { withCredentials: true }),
-  patchUser: ({ id, payload, token }) =>
-    axios.patch(`${process.env.REACT_APP_API_URL}/users/${id}`, payload, {
-      headers: { Authorization: `Bearer ${token}` }
-    }),
+  patchUser: ({ id, payload }) => axios.patch(`/users/${id}`, payload, { withCredentials: true }),
   postUserAvatar: ({ id, payload }) => {
     const data = new FormData();
     data.append("file", payload.avatar);
