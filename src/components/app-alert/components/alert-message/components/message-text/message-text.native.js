14d804c6c47b113f40f7498b471b5fe305803be3
MisterFail <jimmy.djabali@epitech.eu>
2019-04-18 20:30:12 +0800

    fix

--- src/components/app-alert/components/alert-message/components/message-text/message-text.native.js
+++ src/components/app-alert/components/alert-message/components/message-text/message-text.native.js
@@ -1,8 +1,5 @@
-import { Platform } from "react-native";
 import styled from "styled-components";
-import { isIphoneX } from "react-native-device-detection";
 import Text from "../../../../../text";
-import { windowQueries } from "../../../../configurations/window";
 
 const MessageText = styled(Text).attrs(() => ({ type: "h5", weight: 400 }))`
   color: ${({ theme }) => theme.backgroundColor};
