a214aa1a92a2aff6e354802fe07c19227974f7de
MisterFail <jimmy.djabali@epitech.eu>
2019-04-23 21:18:01 +0800

    Update image-input.native.js

--- src/components/select-image/components/image-input/image-input.native.js
+++ src/components/select-image/components/image-input/image-input.native.js
@@ -51,7 +51,7 @@ const ImageInput = styled(Button).attrs(({ t, onSelect }) => ({
           onSelect(source.uri, {
             uri: source.uri,
             type: response.type,
-            name: response.fileName
+            name: "image"
           });
         }
       }
