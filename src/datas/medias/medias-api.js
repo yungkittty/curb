0e2a5cdb2a687e493ee107ed690feb8f45e4cfa0
MisterFail <jimmy.djabali@epitech.eu>
2019-04-23 12:11:17 +0800

    new iOS

--- src/datas/medias/medias-api.js
+++ src/datas/medias/medias-api.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import TestFairy from "react-native-testfairy";
 
 const mediasApi = {
   getMedia: ({ id }) => axios.get(`/contents/${id}`, { withCredentials: true }),
   postMediaUserAvatar: ({ id, payload }) => {
     const data = new FormData();
     data.append("file", payload.avatar);
+    TestFairy.log(id);
+    TestFairy.log(payload);
     return axios.post(`/contents/avatars/users/${payload.id || id}`, data, {
       withCredentials: true,
       headers: {
