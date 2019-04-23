754119c21aa5aae6d5edad613798ffce8fb5169c
MisterFail <jimmy.djabali@epitech.eu>
2019-04-23 14:37:38 +0800

    remove headers

--- src/datas/medias/medias-api.js
+++ src/datas/medias/medias-api.js
@@ -1,20 +1,12 @@
 import axios from "axios";
-import TestFairy from "react-native-testfairy";
 
 const mediasApi = {
   getMedia: ({ id }) => axios.get(`/contents/${id}`, { withCredentials: true }),
   postMediaUserAvatar: ({ id, payload }) => {
     const data = new FormData();
     data.append("file", payload.avatar);
-    TestFairy.log(id);
-    TestFairy.log(payload);
-    const body = Object.setPrototypeOf(data, null);
-    return axios.post(`/contents/avatars/users/${payload.id || id}`, body, {
-      withCredentials: true,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "multipart/form-data"
-      }
+    return axios.post(`/contents/avatars/users/${payload.id || id}`, data, {
+      withCredentials: true
     });
   }
 };
