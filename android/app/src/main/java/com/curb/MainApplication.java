73c910bf1a98470364d4bd91ed5b6f5f248a9b35
MisterFail <jimmy.djabali@epitech.eu>
2019-04-25 17:10:49 +0800

    react-native-fast-image

--- android/app/src/main/java/com/curb/MainApplication.java
+++ android/app/src/main/java/com/curb/MainApplication.java
@@ -3,6 +3,7 @@ package com.curb;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dylanvann.fastimage.FastImageViewPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FastImageViewPackage(),
             new ImagePickerPackage(),
             new ReactNativeI18n(),
             new VectorIconsPackage(),
