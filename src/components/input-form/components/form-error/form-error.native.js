4fc46972536b62c92658acf49161acbd6705baef
MisterFail <jimmy.djabali@epitech.eu>
2019-04-26 16:13:46 +0800

    no differenciation between Android & iOS anymore on FormError

--- src/components/input-form/components/form-error/form-error.native.js
+++ src/components/input-form/components/form-error/form-error.native.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
-import { Platform } from "react-native";
 import Text from "../../../text";
 
 const FormError = styled(Text)`
   position: absolute;
   left: 0px;
-  bottom: ${Platform.OS === "android" ? "-18" : "-10"}px
+  bottom: -18px;
   color: ${props => props.theme.errorColor};
 `;
 
