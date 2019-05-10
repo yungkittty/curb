5dfb0cb5ec976f87ca12d2877b18ad9f46cef83c
MisterFail <jimmy.djabali@epitech.eu>
2019-05-10 22:04:38 +0800

    Update medias-api.js

--- src/datas/medias/medias-api.js
+++ src/datas/medias/medias-api.js
@@ -21,18 +21,7 @@ const mediasApi = {
   postGroupVideoContent: ({ groupId, userId, video, onUploadProgress }) => {
     const data = new FormData();
     data.append("file", video);
-    console.log(groupId);
-    console.log(userId);
-    console.log(video);
     return axios.post(`/contents/videos/${groupId}/${userId}`, data, {
-      withCredentials: true,
-      onUploadProgress
-    });
-  },
-  postMediaAvatarGroup: ({ id, avatar, onUploadProgress }) => {
-    const data = new FormData();
-    data.append("file", avatar.value.file);
-    return axios.post(`/contents/avatars/groups/${id}`, data, {
       withCredentials: true,
       onUploadProgress
     });
