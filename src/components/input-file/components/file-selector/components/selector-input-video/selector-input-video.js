f6fedcae51906a8fcbc8bc4d9b0d443e753eb0d5
MisterFail <jimmy.djabali@epitech.eu>
2019-05-13 13:30:07 +0800

    lasts fixs

--- src/components/input-file/components/file-selector/components/selector-input-video/selector-input-video.js
+++ src/components/input-file/components/file-selector/components/selector-input-video/selector-input-video.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const SelectorInputVideo = styled.input.attrs(({ onSelect }) => ({
   type: "file",
-  accept: "video/mp4, video/avi",
+  accept: "video/mp4, video/avi", "video/mov",
   onChange: evt => {
     const file = evt.target.files[0];
     const reader = new FileReader();
