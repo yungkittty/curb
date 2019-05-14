1c0483d121dae8054c3137919abb3e52dc8e797e
Hugo Toussaint <hugo.toussaint@epitech.eu>
2019-05-15 00:30:05 +0200

    font family update

--- src/components/input-area/input.js
+++ src/components/input-area/input.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 // https://github.com/yungkittty/curb/blob/develop/src/components/text/text.js
 
 const TextArea = styled.textarea`
+  font-family: "Montserrat-Regular";
   padding: 18px;
   font-size: 18px;
   border-top: none;
