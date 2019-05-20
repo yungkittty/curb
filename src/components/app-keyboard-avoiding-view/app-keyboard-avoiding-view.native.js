1165ee15a2446f3f56fab380235196c4a1946201
Quentin Woivré <quentin.woivre@epitech.eu>
2019-05-20 17:45:49 +0800

    app-kayboard, ...

--- src/components/app-keyboard-avoiding-view/app-keyboard-avoiding-view.native.js
+++ src/components/app-keyboard-avoiding-view/app-keyboard-avoiding-view.native.js
@@ -2,7 +2,7 @@ import React from "react";
 import { KeyboardAvoidingView } from "react-native";
 
 const AppKeyboardAvoidingView = WrappedComponent => props => (
-  <KeyboardAvoidingView>
+  <KeyboardAvoidingView behavior="padding">
     <WrappedComponent {...props} />
   </KeyboardAvoidingView>
 );
