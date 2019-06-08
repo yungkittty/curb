ad401e8710842bba5d933c8480b954f9388bd902
Quentin Woivré <quentin.woivre@epitech.eu>
2019-06-08 18:41:44 +0200

    placeholders, zipper-rule, ...

--- src/scenes/group/components/group-list-item-media/components/item-header/components/header-infos/components/infos-subtitle/infos-subtitle.js
+++ src/scenes/group/components/group-list-item-media/components/item-header/components/header-infos/components/infos-subtitle/infos-subtitle.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import Text from "../../../../../../../../../../components/text";
+import Container from "../../../../../../../../../../components/container";
 
-const InfosSubtitle = styled(Text)`
+const InfosSubtitle = styled(Text).attrs(props => ({ as: !props.mediaDateCreation ? Container : undefined }))`
   ${props => (!props.mediaDateCreation ? `width: 60px; height: 12px;` : "")}
   border-radius: 5px;
   color: ${props => props.theme.secondaryVariantColor};
--- src/scenes/group/components/group-list-item-media/components/item-header/components/header-infos/components/infos-title/infos-title.js
+++ src/scenes/group/components/group-list-item-media/components/item-header/components/header-infos/components/infos-title/infos-title.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import Text from "../../../../../../../../../../components/text";
+import Container from "../../../../../../../../../../components/container";
 
-const InfosTitle = styled(Text)`
+const InfosTitle = styled(Text).attrs(props => ({ as: !props.userName ? Container : undefined }))`
   ${props => (!props.userName ? `width: 120px; height: 14px;` : "")}
   border-radius: 5px;
   background-color: ${props => (!props.userName ? props.theme.primaryVariantColor : "transparent")};
