cbd499671fca6a0aa02bdc82f93352684706e6b2
Quentin Woivré <quentin.woivre@epitech.eu>
2019-06-09 18:41:35 +0200

    list-section, ...

--- src/components/list-section/list-section.native.js
+++ src/components/list-section/list-section.native.js
@@ -1,3 +1,15 @@
-import { SectionList as ListSection } from "react-native";
+import React from "react";
+import PropTypes from "prop-types";
+import { SectionList } from "react-native";
+
+const ListSection = props => <SectionList {...props} />;
+
+ListSection.defaultProps = {
+  stickySectionHeadersEnabled: false
+};
+
+ListSection.propTypes = {
+  stickySectionHeadersEnabled: PropTypes.bool
+};
 
 export default ListSection;
