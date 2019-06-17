15d9729fb221f743cd81b0b737aad822f1e571b7
MisterFail <jimmy.djabali@epitech.eu>
2019-06-17 20:55:28 +0200

    send preview url to discord

--- .travis/deploy-web.sh
+++ .travis/deploy-web.sh
@@ -8,6 +8,8 @@ NETLIFY_OUTPUT=$(netlify deploy $([[ $CURB_VERSION == "develop" ]] && echo "--pr
 if [[ "$CURB_BUILD" != "production" && "$CURB_VERSION" != "develop" ]]; then
     DEPLOY_PREVIEW_URL="https://$( echo "${NETLIFY_OUTPUT%x}" | grep -o 'Live Draft URL:.*' | cut -f3- -d/ )"
 
+    bash ../.travis/send-discord-logs.sh $WEBHOOK_URL
+
     curl -q -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
     -d " { \"body\": \"Preview URL:\n$DEPLOY_PREVIEW_URL\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
