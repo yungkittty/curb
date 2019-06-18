978f47925eb4f138ad41b77450829fcab8c826a1
MisterFail <jimmy.djabali@epitech.eu>
2019-06-18 20:42:38 +0200

    managing new development cert

--- .travis/build-ios.sh
+++ .travis/build-ios.sh
@@ -5,7 +5,7 @@ set -xe
 
 # Decodes certificates and mobile provision.
 openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in curb-developer.cer.enc -d -a -out curb-developer.cer
-openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in curb-development-key.cer.p12.enc -d -a -out curb-development-key.cer.p12
+openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in curb-development-key.cer.p12.enc -d -a -out curb-development-key_old.cer.p12
 openssl aes-256-cbc -k "$ENCRYPT_PASSWORD" -in curb.mobileprovision.enc -d -a -out curb_old.mobileprovision
 
 # Creates keychain to store certificates.
@@ -17,7 +17,7 @@ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 # Imports certificates to keychain.
 security import curb-apple.cer -k ios-build.keychain -A
 security import curb-developer.cer -k ios-build.keychain -A
-security import curb-development-key.cer.p12 -k ios-build.keychain -P "$DEV_KEY_PASSWORD" -A
+security import curb-development-key.cer.p12 -k ios-build.keychain -P "$DEV_KEY_PASSWORD_NEW" -A
 
 # Allows codesigning.
 # https://stackoverflow.com/a/40039594
@@ -28,5 +28,5 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp curb.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
 
 # Builds .xcarchive using `Release` configuration, then create .ipa from it.
-xcodebuild -quiet archive -project curb.xcodeproj -scheme curb -configuration Release -archivePath curb.xcarchive PROVISIONING_PROFILE="01e34f5f-a6bf-4c4c-aa50-5a9c5d4d7c97" CODE_SIGN_IDENTITY="iPhone Developer" PROVISIONING_PROFILE_SPECIFIER="2020 Curb"
+xcodebuild -quiet archive -project curb.xcodeproj -scheme curb -configuration Release -archivePath curb.xcarchive
 xcodebuild -quiet -exportArchive -archivePath curb.xcarchive -exportOptionsPlist exportOptions.plist -exportPath "export"
