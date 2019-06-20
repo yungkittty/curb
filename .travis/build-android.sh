57e080315c3d589154220ba19de8bcbc37773574
MisterFail <jimmy.djabali@epitech.eu>
2019-06-20 17:00:35 +0200

    new keystore, com.curb -> com.curb.eip

--- .travis/build-android.sh
+++ .travis/build-android.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Decodes keystore. (eg. /app/build.gradle)
-openssl aes-256-cbc -k "$CURB_STORE_ENCRYPT_PASSWORD" -in app/curb.keystore.enc -d -a -out app/curb.keystore
+openssl aes-256-cbc -k "$CURB_STORE_ENCRYPT_PASSWORD_NEW" -in app/curb.keystore.enc -d -a -out app/curb.keystore
 
 # Builds .apk using `Release` configuration.
 ./gradlew -q -x bundleReleaseJsAndAssets assembleRelease
