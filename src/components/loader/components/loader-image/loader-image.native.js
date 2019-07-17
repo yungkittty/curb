5666d02626d4cc12411c409c9460c663924ae088
MisterFail <jimmy.djabali@epitech.eu>
2019-07-17 21:24:23 +0200

    Revert "changed values"
    This reverts commit 74f4d0535ec538878021c5c84392995c99780961.

--- src/components/loader/components/loader-image/loader-image.native.js
+++ src/components/loader/components/loader-image/loader-image.native.js
@@ -17,8 +17,7 @@ class LoaderImage extends Component {
       Animated.timing(opacity, {
         toValue: 2,
         easing: Easing.ease,
-        duration: 1700,
-        useNativeDriver: true
+        duration: 1300
       })
     ).start();
   }
@@ -31,8 +30,7 @@ class LoaderImage extends Component {
         style={{
           opacity: opacity.interpolate({
             inputRange: [0, 1, 2],
-            outputRange: [1, 0, 1],
-            useNativeDriver: true
+            outputRange: [1, 0, 1]
           })
         }}
       >
