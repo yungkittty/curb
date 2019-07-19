48a161b3ec907f39ee9874c6c37669998ad9af2f
Quentin Woivré <quentin.woivre@epitech.eu>
2019-07-19 16:47:40 +0200

    fonts, ...

--- src/configurations/internationalisation/translations/en/en-discovery.js
+++ src/configurations/internationalisation/translations/en/en-discovery.js
@@ -2,7 +2,7 @@ const enDiscovery = {
   title: "Discovery",
   subtitle: "Join popular groups on the network",
   sections: {
-    global: "Most Populars",
+    global: "Most Popular",
     custom: "Recommended",
     random: "Random"
   }
