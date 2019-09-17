4058b509128cd36dbb97b00df8d87966cfc93eb8
MisterFail <jimmy.djabali@epitech.eu>
2019-09-17 21:24:58 +0200

    Update discovery.js

--- src/scenes/discovery/discovery.js
+++ src/scenes/discovery/discovery.js
@@ -1,6 +1,5 @@
 import _ from "lodash";
 import React from "react";
-import PropTypes from "prop-types";
 import { withTranslation } from "react-i18next";
 import withAppModal from "../../hocs/with-app-modal";
 import withCurrentUser from "../../hocs/with-current-user";
@@ -10,8 +9,8 @@ import Loader from "../../components/loader";
 /* eslint-disable */
 
 import Container from "../../components/container";
-import Image from "../../components/image";
 import Video from "../../components/video";
+import ImageGallery from "../../components/image-gallery";
 
 /* eslint-enable */
 
@@ -20,48 +19,37 @@ class Discovery extends React.Component {
     super(props);
 
     this.state = { mediaList: {} };
-
-    this.onChangeText = this.onChangeText.bind(this);
-    this.addImage = this.addImage.bind(this);
-    this.addVideo = this.addVideo.bind(this);
-
-    this.removeContent = this.removeContent.bind(this);
-  }
-
-  onChangeText({ target: { value } }) {
-    const { mediaList } = this.state;
-    this.setState({
-      mediaList: value !== "" ? { ...mediaList, text: value } : { ..._.omit(mediaList, "text") }
-    });
   }
 
   addImage(imageData) {
-    const {
-      mediaList,
-      mediaList: { image = [] }
-    } = this.state;
+    const { imageList = [], mediaList } = this.state;
+    const newImageList = [...imageList, imageData];
     this.setState({
-      mediaList: { ...mediaList, image: [...image, imageData] }
+      imageList: newImageList,
+      mediaList: { ...mediaList, image: <ImageGallery imagesData={newImageList} /> }
     });
   }
 
   addVideo(videoData) {
     const { mediaList } = this.state;
-    //    console.log(mediaList);
     this.setState({
-      mediaList: { ...mediaList, video: videoData }
+      mediaList: { ...mediaList, video: <Video style={{ flex: 1 }} src={videoData} /> }
     });
   }
 
   removeContent(mediaType) {
-    const { mediaList } = this.state;
-    mediaList[mediaType].pop();
-    if (_.size(mediaList[mediaType]) === 0) delete mediaList[mediaType];
-    //    console.log(mediaList);
+    const { mediaList, imageList } = this.state;
+
+    if (mediaType === "image") {
+      imageList.pop();
+      this.setState({ imageList });
+      if (_.size(imageList) === 0) delete mediaList.image;
+    } else delete mediaList[mediaType];
     this.setState({ mediaList });
   }
 
   render() {
+    const { mediaList } = this.state;
     return (
       <Container
         style={{
@@ -74,39 +62,34 @@ class Discovery extends React.Component {
       >
         <Card
           userId="5d373369c8acd2001d90bf55"
+          onFloatingButtonClick={() => console.log("toto")}
+          floatingButtonDisabled={!mediaList.text || mediaList.text.length === 0}
+          floatingButtonColor="blue"
+          /*
+          dropdownMenu={{
+            icon: "ellipsis-v",
+            optionsList: [
+              { text: "Pin", icon: "thumbtack", onClick: () => console.log("report") },
+              { text: "Delete", icon: "trash", onClick: () => console.log("report") },
+              { text: "Report", icon: "flag", onClick: () => console.log("report") }
+            ]
+          }}
+          */
           dropdownMenu={{
             icon: "minus",
-            onClick: mediaType => console.log(`remove the ${mediaType} component`)
+            onClick: this.removeContent
           }}
           postMediaTypes={[
-            { type: "text", onChange: () => console.log("text") },
-            {
-              type: "event",
-              onClick: () => console.log("add event to mediaList")
-            },
-            { type: "poll", onClick: () => console.log("add poll to mediaList") }
+            { type: "image", onSelect: this.addImage },
+            { type: "video", onSelect: this.addVideo },
+            { type: "text", onChange: this.onChangeText }
           ]}
-          mediaList={{ event: <Loader />, poll: <Loader /> }}
+          mediaList={mediaList}
         />
       </Container>
     );
   }
 }
-
-Discovery.propTypes = {
-  showAppModal: PropTypes.func.isRequired,
-  currentUserId: PropTypes.string.isRequired,
-  isDiscoveryGlobalSectionEnd: PropTypes.bool.isRequired,
-  isDiscoveryCustomSectionEnd: PropTypes.bool.isRequired,
-  isDiscoveryRandomSectionEnd: PropTypes.bool.isRequired,
-  discoveryGlobalSectionGroupsId: PropTypes.array.isRequired, // eslint-disable-line
-  discoveryCustomSectionGroupsId: PropTypes.array.isRequired, // eslint-disable-line
-  discoveryRandomSectionGroupsId: PropTypes.array.isRequired, // eslint-disable-line
-  getDiscoveryGlobalSectionGroupsId: PropTypes.func.isRequired,
-  getDiscoveryCustomSectionGroupsId: PropTypes.func.isRequired,
-  getDiscoveryRandomSectionGroupsId: PropTypes.func.isRequired,
-  t: PropTypes.func.isRequired
-};
 
 export default _.flowRight([
   // eslint-disable-line
