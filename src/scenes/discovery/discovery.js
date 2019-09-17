1d42fa931a8b6516d16d2efc4ff26bcf298c9485
MisterFail <jimmy.djabali@epitech.eu>
2019-09-18 00:14:24 +0200

    Update discovery.js

--- src/scenes/discovery/discovery.js
+++ src/scenes/discovery/discovery.js
@@ -1,177 +1,140 @@
 import _ from "lodash";
 import React from "react";
-import PropTypes from "prop-types";
 import { withTranslation } from "react-i18next";
-import ListSection from "../../components/list-section";
-import DiscoveryHeader from "./components/discovery-header";
-import DiscoveryListSectionHeader from "./components/discovery-list-section-header";
-import DiscoveryList from "./components/discovery-list";
-import DiscoveryListItem from "./components/discovery-list-item";
-import ButtonFloat from "../../components/button-float";
 import withAppModal from "../../hocs/with-app-modal";
 import withCurrentUser from "../../hocs/with-current-user";
+import Card from "../../components/card";
 
 /* eslint-disable */
 
-import GroupCreate from "../group/scenes/group-create";
+import ContainerScroll from "../../components/container-scroll";
+import ImageGallery from "../../components/image-gallery";
+import Video from "../../components/video";
 
 /* eslint-enable */
 
 class Discovery extends React.Component {
   constructor(props) {
     super(props);
-    this.getSection = this.getSection.bind(this);
-    this.getSections = this.getSections.bind(this);
-    this.renderListHeader = this.renderListHeader.bind(this);
-    this.renderListSectionHeader = this.renderListSectionHeader.bind(this);
-    this.renderListSectionItem = this.renderListSectionItem.bind(this);
-    this.renderListItem = this.renderListItem.bind(this);
+
+    this.state = { mediaList: {} };
+
+    this.onChangeText = this.onChangeText.bind(this);
+    this.addImage = this.addImage.bind(this);
+    this.addVideo = this.addVideo.bind(this);
+    this.removeContent = this.removeContent.bind(this);
   }
 
-  // eslint-disable-next-line
-  getSection(
-    isDiscoverySectionEnd,
-    discoverySectionTitle,
-    discoverySectionGroupsId,
-    getDiscoverySectionGroupsId,
-    isShowed = true
-  ) {
-    return isShowed ? [{
-      isDiscoverySectionEnd,
-      discoverySectionTitle,
-      data: [{ key: discoverySectionTitle, discoverySectionGroupsId }],
-      getDiscoverySectionGroupsId
-    }] : [];
+  onChangeText({ target: { value } }) {
+    const { mediaList } = this.state;
+    this.setState({
+      mediaList: value !== "" ? { ...mediaList, text: value } : { ..._.omit(mediaList, "text") }
+    });
   }
 
-  getSections() {
-    const {
-      // eslint-disable-line
-      currentUserId,
-      isDiscoveryGlobalSectionEnd,
-      isDiscoveryCustomSectionEnd,
-      isDiscoveryRandomSectionEnd,
-      discoveryGlobalSectionGroupsId,
-      discoveryCustomSectionGroupsId,
-      discoveryRandomSectionGroupsId,
-      getDiscoveryGlobalSectionGroupsId,
-      getDiscoveryCustomSectionGroupsId,
-      getDiscoveryRandomSectionGroupsId
-    } = this.props;
-    return [
-      ...this.getSection(
-        // eslint-disable-line
-        isDiscoveryGlobalSectionEnd,
-        "global",
-        discoveryGlobalSectionGroupsId,
-        getDiscoveryGlobalSectionGroupsId
-      ),
-      ...this.getSection(
-        // eslint-disable-line
-        isDiscoveryCustomSectionEnd,
-        "custom",
-        discoveryCustomSectionGroupsId,
-        getDiscoveryCustomSectionGroupsId,
-        !!currentUserId
-      ),
-      ...this.getSection(
-        // eslint-disable-line
-        isDiscoveryRandomSectionEnd,
-        "random",
-        discoveryRandomSectionGroupsId,
-        getDiscoveryRandomSectionGroupsId
-      )
-    ];
+  addImage(imageData) {
+    const { imageList = [], mediaList } = this.state;
+    const newImageList = [...imageList, imageData];
+    this.setState({
+      imageList: newImageList,
+      mediaList: { ...mediaList, image: <ImageGallery imagesData={newImageList} /> }
+    });
   }
 
-  renderListHeader() {
-    const { t } = this.props;
-    return (
-      <DiscoveryHeader
-        // eslint-disable-line
-        t={t}
-      />
-    );
+  addVideo(videoData) {
+    const { mediaList } = this.state;
+    this.setState({
+      mediaList: { ...mediaList, video: <Video style={{ flex: 1 }} src={videoData} /> }
+    });
   }
 
-  renderListSectionHeader({ section: discoverySectionData }) {
-    const { t } = this.props;
-    return (
-      <DiscoveryListSectionHeader type="h3" weight={500}>
-        {t(`sections.${discoverySectionData.discoverySectionTitle}`)}
-      </DiscoveryListSectionHeader>
-    );
-  }
+  removeContent(mediaType) {
+    const { mediaList, imageList } = this.state;
 
-  renderListSectionItem({
-    // eslint-disable-line
-    item: discoveryItemData,
-    section: discoverySectionData
-  }) {
-    const { discoverySectionGroupsId } = discoveryItemData;
-    const { isDiscoverySectionEnd, getDiscoverySectionGroupsId } = discoverySectionData;
-    return (
-      <DiscoveryList
-        isDiscoverySectionEnd={isDiscoverySectionEnd}
-        discoverySectionGroupsId={discoverySectionGroupsId}
-        getDiscoverySectionGroupsId={getDiscoverySectionGroupsId}
-        keyExtractor={discoverySectionGroupId => discoverySectionGroupId}
-        renderItem={this.renderListItem}
-      />
-    );
-  }
-
-  // eslint-disable-next-line
-  renderListItem({ item: discoveryItemId }) {
-    return (
-      <DiscoveryListItem
-        // eslint-disable-line
-        shouldFetch={false}
-        groupId={discoveryItemId}
-      />
-    );
+    if (mediaType === "image") {
+      imageList.pop();
+      this.setState({ imageList });
+      if (_.size(imageList) === 0) delete mediaList.image;
+    } else delete mediaList[mediaType];
+    this.setState({ mediaList });
   }
 
   render() {
-    const { showAppModal, currentUserId } = this.props;
+    const { t } = this.props;
+    const { mediaList } = this.state;
+    const postMediaTypes = [
+      { type: "text", onChange: this.onChangeText },
+      { type: "image", onSelect: this.addImage }
+    ];
+    if (!mediaList.video) postMediaTypes.push({ type: "video", onSelect: this.addVideo });
     return (
-      <React.Fragment>
-        <ListSection
-          sections={this.getSections()}
-          ListHeaderComponent={this.renderListHeader}
-          renderSectionHeader={this.renderListSectionHeader}
-          renderItem={this.renderListSectionItem}
+      <ContainerScroll
+        style={{ flexFlow: "column", flex: 1 }}
+        contentContainerStyle={{ alignItems: "center" }}
+      >
+        <Card
+          style={{ marginTop: 40 }}
+          userId="5d373369c8acd2001d90bf55"
+          dropdownMenu={{ icon: "minus", onClick: this.removeContent }}
+          postMediaTypes={postMediaTypes}
+          mediaList={mediaList}
         />
-        {currentUserId ? (
-          <ButtonFloat
-            // eslint-disable-line
-            icon="plus"
-            onClick={() => showAppModal({ scene: GroupCreate })}
-          />
-        ) : null}
-      </React.Fragment>
+        <Card
+          style={{ marginTop: 40, marginBottom: 40 }}
+          userId="5d373369c8acd2001d90bf55"
+          dropdownMenu={{
+            icon: "ellipsis-v",
+            optionsList: [
+              { text: t("pin"), icon: "thumbtack", onClick: () => console.log("pin") },
+              { text: t("delete"), icon: "trash", onClick: () => console.log("pin") },
+              { text: t("report"), icon: "flag", onClick: () => console.log("pin") }
+            ]
+          }}
+          mediaList={{
+            image: (
+              <ImageGallery
+                imagesData={[
+                  "http://livingplatform.co/wp/greenwich/wp-content/uploads/sites/2/2019/03/photo-1520045892732-304bc3ac5d8e.jpeg",
+                  "https://theboardr.blob.core.windows.net/eventsicons/1063.jpg",
+                  "https://www.sitegallery.org/app/uploads/2019/08/NC-Sun-LR-@garryjonesphotography-84-1600x1066.jpg"
+                ]}
+              />
+            ),
+            video: <Video style={{ flex: 1 }} src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" />
+          }}
+        />
+        <Card
+          style={{ marginTop: 40, marginBottom: 40 }}
+          userId="5d373369c8acd2001d90bf55"
+          dropdownMenu={{
+            icon: "ellipsis-v",
+            optionsList: [
+              { text: t("pin"), icon: "thumbtack", onClick: () => console.log("pin") },
+              { text: t("delete"), icon: "trash", onClick: () => console.log("pin") },
+              { text: t("report"), icon: "flag", onClick: () => console.log("pin") }
+            ]
+          }}
+          mediaList={{
+            text: "J'ai adoré Paris. C'était vraiment magnifique !",
+            image: (
+              <ImageGallery
+                imagesData={[
+                  "https://www.pariscityvision.com/library/image/1668",
+                  "https://www.parisinfo.com/var/otcp/sites/images/_aliases/article_full/node_43/arc-de-triomphe-au-soleil-couchant-%7C-630x405-%7C-%C2%A9-willian-west/18130652-1-fre-FR/Arc-de-Triomphe-au-soleil-couchant-%7C-630x405-%7C-%C2%A9-Willian-West.jpg"
+                ]}
+              />
+            ),
+            video: <Video style={{ flex: 1 }} src="http://techslides.com/demos/sample-videos/small.mp4" />
+          }}
+        />
+      </ContainerScroll>
     );
   }
 }
-
-Discovery.propTypes = {
-  showAppModal: PropTypes.func.isRequired,
-  currentUserId: PropTypes.string.isRequired,
-  isDiscoveryGlobalSectionEnd: PropTypes.bool.isRequired,
-  isDiscoveryCustomSectionEnd: PropTypes.bool.isRequired,
-  isDiscoveryRandomSectionEnd: PropTypes.bool.isRequired,
-  discoveryGlobalSectionGroupsId: PropTypes.array.isRequired, // eslint-disable-line
-  discoveryCustomSectionGroupsId: PropTypes.array.isRequired, // eslint-disable-line
-  discoveryRandomSectionGroupsId: PropTypes.array.isRequired, // eslint-disable-line
-  getDiscoveryGlobalSectionGroupsId: PropTypes.func.isRequired,
-  getDiscoveryCustomSectionGroupsId: PropTypes.func.isRequired,
-  getDiscoveryRandomSectionGroupsId: PropTypes.func.isRequired,
-  t: PropTypes.func.isRequired
-};
 
 export default _.flowRight([
   // eslint-disable-line
   withAppModal,
   withCurrentUser,
-  withTranslation("discovery")
+  withTranslation("common")
 ])(Discovery);
