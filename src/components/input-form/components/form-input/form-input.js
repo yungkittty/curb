09fc704de515ffa66c4e909da07d54fe2ea1696c
MisterFail <jimmy.djabali@epitech.eu>
2019-09-23 23:55:56 +0200

    fixed margin problem on SignIn

--- src/components/input-form/components/form-input/form-input.js
+++ src/components/input-form/components/form-input/form-input.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Input from "../../../input";
 
 const FormInput = styled(Input)`
+  width: 100%;
   display: flex;
   align-content: center;
   justify-content: center;
