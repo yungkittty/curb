5abb1d9c5d5484d1f626b8d6c5c82e025181728b
MisterFail <jimmy.djabali@epitech.eu>
2019-10-24 15:28:44 +0200

    using with-shadow hoc

--- src/components/card/components/card-border-container/card-border-container.js
+++ src/components/card/components/card-border-container/card-border-container.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
+import withShadow from "../../../../hocs/with-shadow";
 import Container from "../../../container";
-
-// https://github.com/alekhurst/react-native-elevated-view/blob/master/index.js#L33 // 4
 
 const CardBorderContainer = styled(Container)`
   position: relative;
@@ -9,7 +8,6 @@ const CardBorderContainer = styled(Container)`
   overflow: hidden;
   flex: 1;
   background-color: white;
-  box-shadow: 0px 2.4px 2.16px 0px rgba(0, 0, 0, 0.186);
 `;
 
-export default CardBorderContainer;
+export default withShadow(4)(CardBorderContainer);
--- src/components/card/components/card-floating-button/components/button-container/button-container.native.js
+++ src/components/card/components/card-floating-button/components/button-container/button-container.native.js
@@ -1,8 +1,6 @@
-import { Platform } from "react-native";
 import styled from "styled-components";
+import withShadow from "../../../../../../hocs/with-shadow";
 import Button from "../../../../../button";
-
-// https://github.com/alekhurst/react-native-elevated-view/blob/master/index.js#L33 // 6
 
 const ButtonContainer = styled(Button)`
   position: absolute;
@@ -10,17 +8,6 @@ const ButtonContainer = styled(Button)`
   right: 20px;
   flex-flow: column;
   background: white;
-  z-index: 6;
-  ${Platform.OS === "android"
-    ? `
-    elevation: 6;
-  `
-    : `
-    shadow-offset: 0px 3.6px;
-    shadow-radius: 3.24px;
-    shadow-color: rgba(0, 0, 0, 1);
-    shadow-opacity: 0.189;
-  `}
 `;
 
-export default ButtonContainer;
+export default withShadow(4)(ButtonContainer);
