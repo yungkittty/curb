12cd6c0e41d2b04a20ad71385181d3753f796b17
MisterFail <jimmy.djabali@epitech.eu>
2019-10-25 13:32:42 +0200

    update discovery

--- src/scenes/discovery/discovery.js
+++ src/scenes/discovery/discovery.js
@@ -84,15 +84,19 @@ class Discovery extends React.Component {
           mediaList={mediaList}
         />
         <Card
-          style={{ marginTop: platformBools.isWeb ? 40 : 30 }}
+          style={{ marginTop: platformBools.isWeb ? 60 : 50 }}
           userId="5d89f888d93e22001db00053"
           cardMenu={[
             { text: t("common:delete"), icon: "trash", onClick: () => console.log("pin") },
             { text: t("common:report"), icon: "flag", onClick: () => console.log("pin") }
           ]}
+          mediaDateCreation="2019-09-27T13:38:01.240Z"
+          onFloatingButtonClick={() => console.log("toto")}
+          likeNumber="3 K"
+          floatingButtonColor="rgb(86, 204, 242)"
           mediaList={{
             text:
-              "Nous partageons sur ce post des plans ride localisés par la communauté sur toute la France. Vous pouvez également y partager vos photos et vidéos de vos tricks et suivre ceux des autres.",
+              "Nous partageons sur ce post des plans ride localisés par la communauté pouvez également y partager vos photos et vidéos de vos tricks et suivre ceux des autres. Nous partageons sur ce post des plans ride localisés par la communauté sur toute la France. Vous pouvez également y partager vos photos et vidéos de vos tricks et suivre ceux des autres. Nous partageons sur ce post des plans ride localisés par la communauté sur toute la France. Vous pouvez également y partager vos photos et vidéos de vos tricks et suivre ceux des autres. Nous partageons sur ce post des plans ride localisés par la communauté sur toute la France. Vous pouvez également y partager vos photos et vidéos de vos tricks et suivre ceux des autres par exemple",
             image: (
               <ImageGallery
                 imagesData={[
@@ -101,17 +105,19 @@ class Discovery extends React.Component {
                   "https://www.sitegallery.org/app/uploads/2019/08/NC-Sun-LR-@garryjonesphotography-84-1600x1066.jpg"
                 ]}
               />
-            ),
+            )
+            /*
             video: (
               <Video
                 style={{ height: "100%", width: "100%" }}
                 src="https://www.videvo.net/videvo_files/converted/2015_08/preview/Slowmotion_closeup_of_skateboard.mp442512.webm"
               />
             )
+            */
           }}
         />
         <Card
-          style={{ marginTop: platformBools.isWeb ? 40 : 30 }}
+          style={{ marginTop: platformBools.isWeb ? 60 : 50 }}
           userId="5d89f888d93e22001db00053"
           onFloatingButtonClick={() => console.log("toto")}
           likeNumber="541"
@@ -121,13 +127,14 @@ class Discovery extends React.Component {
             { text: t("common:delete"), icon: "trash", onClick: () => console.log("pin") },
             { text: t("common:report"), icon: "flag", onClick: () => console.log("pin") }
           ]}
+          mediaDateCreation="2019-09-27T13:38:01.240Z"
           mediaList={{
             text:
-              "J'ai adoré Paris. C'était vraiment magnifique ! Il y a pleins de musées, de belles avenues, et de shopping à faire."
+              "Nous partageons sur ce post des plans ride localisés par la communauté sur toute la France. Vous pouvez également y partager vos photos et vidéos de vos tricks et suivre ceux des autres. Nous partageons sur ce post des plans ride localisés par la communauté sur toute la France. Vous pouvez également y partager vos photos et vidéos de vos tricks et suivre ceux des autres. Nous partageons sur ce post des plans ride localisés par la communauté sur toute la France. Vous pouvez également y partager vos photos et vidéos de vos tricks et suivre ceux des autres. Nous partageons sur ce post des plans ride localisés par la communauté sur toute la France. Vous pouvez également partager vos photos et vidéos de vos tricks, ou suivre ceux des autres par exemple."
           }}
         />
         <Card
-          style={{ marginTop: platformBools.isWeb ? 40 : 30 }}
+          style={{ marginTop: platformBools.isWeb ? 60 : 50 }}
           userId="5d89f888d93e22001db00053"
           onFloatingButtonClick={() => console.log("toto")}
           likeNumber="2 K"
@@ -137,6 +144,7 @@ class Discovery extends React.Component {
             { text: t("common:delete"), icon: "trash", onClick: () => console.log("pin") },
             { text: t("common:report"), icon: "flag", onClick: () => console.log("pin") }
           ]}
+          mediaDateCreation="2019-09-27T13:38:01.240Z"
           mediaList={{
             text: "J'ai adoré Paris. C'était vraiment magnifique !",
             image: (
@@ -146,17 +154,19 @@ class Discovery extends React.Component {
                   "https://www.parisinfo.com/var/otcp/sites/images/_aliases/article_full/node_43/arc-de-triomphe-au-soleil-couchant-%7C-630x405-%7C-%C2%A9-willian-west/18130652-1-fre-FR/Arc-de-Triomphe-au-soleil-couchant-%7C-630x405-%7C-%C2%A9-Willian-West.jpg"
                 ]}
               />
-            ),
+            )
+            /*
             video: (
               <Video
                 style={{ height: "100%", width: "100%" }}
                 src="https://www.videvo.net/videvo_files/converted/2015_06/preview/Sacre_Coeur_am_WS_tilt_20150612_01_Videvo.mov38424.webm"
               />
             )
+            */
           }}
         />
         <Card
-          style={{ marginTop: platformBools.isWeb ? 40 : 30 }}
+          style={{ marginTop: platformBools.isWeb ? 60 : 50 }}
           userId="5d89f888d93e22001db00053"
           onFloatingButtonClick={() => console.log("toto")}
           likeNumber="345"
@@ -164,23 +174,26 @@ class Discovery extends React.Component {
           cardMenu={[{ text: t("common:report"), icon: "flag", onClick: () => console.log("pin") }]}
           mediaList={{
             text: "We are back very soon...",
+            /*
             video: (
               <Video
                 style={{ height: "100%", width: "100%" }}
                 src="https://raw.githubusercontent.com/mediaelement/mediaelement-files/master/echo-hereweare.mp4"
               />
             ),
+            */
             image: (
               <ImageGallery
                 imagesData={["https://johnmoyermusic.com/wp-content/uploads/2012/10/1-echo1-band.jpg"]}
               />
             )
           }}
+          mediaDateCreation="2019-09-27T13:38:01.240Z"
         />
-        <Card style={{ marginTop: platformBools.isWeb ? 40 : 30 }} groupId="5dac9a1e501e2a001de8e19f" />
+        <Card style={{ marginTop: platformBools.isWeb ? 60 : 50 }} groupId="5dac9a1e501e2a001de8e19f" />
         <Card
           style={
-            platformBools.isWeb ? { marginTop: 40, marginBottom: 40 } : { marginTop: 30, marginBottom: 30 }
+            platformBools.isWeb ? { marginTop: 60, marginBottom: 40 } : { marginTop: 50, marginBottom: 30 }
           }
           groupId="5dac9a7f501e2a001de8e1a2"
         />
