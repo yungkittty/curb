05b2d176eda002f0f97c4474edc6b8646164f5e2
MisterFail <jimmy.djabali@epitech.eu>
2019-10-28 15:31:59 +0100

    Ajouter un nouveau loader. (#144) (#145)
    * new loader
    * changed values
    * Revert "changed values"
    This reverts commit 74f4d0535ec538878021c5c84392995c99780961.
    * Revert "Revert "changed values""
    This reverts commit 5666d02626d4cc12411c409c9460c663924ae088.
    * loader test on web
    * new animation test
    * loader can scale with circle-container's sizes
    * removed logo
    * Update loader-animation-round.js
    * Update loader-animation-round.js
    * New circle system, will work on mobile
    * Update loader-circle.js
    * mobile loading test
    * updated mobile
    * checkout discovery
    * review fixs, updated .env.development
    * Circle -> CircleContainer
    * Update loader-circle-container.native.js
    * Update package.json
    * removed mounted props
    * Update package.json
    * loader-circle-container, ...
    * Fixed loader-circle-container hierarchy, ...

---
+++ src/components/loader/components/loader-circle/loader-circle.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+import CircleContainer from "./components/circle-container";
+import CircleRoundContainer from "./components/circle-round-container";
+import CircleRound from "./components/circle-round";
+
+const LoaderCircle = ({ index, innerDiameter }) => (
+  <CircleContainer index={index} innerDiameter={innerDiameter}>
+    <CircleRoundContainer index={index}>
+      <CircleRound innerDiameter={innerDiameter} />
+    </CircleRoundContainer>
+  </CircleContainer>
+);
+
+LoaderCircle.propTypes = {
+  index: PropTypes.number.isRequired,
+  innerDiameter: PropTypes.number.isRequired
+};
+
+export default LoaderCircle;
