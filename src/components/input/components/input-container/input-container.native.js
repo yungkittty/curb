d32e99797de0c0905b44321caa236e80765eb19c
MisterFail <jimmy.djabali@epitech.eu>
2019-11-23 09:12:37 +0100

    clear() on mobile works

--- src/components/input/components/input-container/input-container.native.js
+++ src/components/input/components/input-container/input-container.native.js
@@ -1,21 +1,15 @@
-import React from "react";
 import styled from "styled-components";
 import { TextInput, Platform } from "react-native";
 
 // https://github.com/yungkittty/curb/blob/develop/src/components/text/text.native.js
 
-const InputContainer = styled(({ forwardedRef, type, onChange, id, isMultiline, ...others }) => (
-  // console.log(forwardedRef);
-  <TextInput
-    {...others}
-    ref={forwardedRef}
-    textAlignVertical="top"
-    keyboardType={type === "email" ? "email-address" : undefined}
-    secureTextEntry={type === "password"}
-    onChangeText={value => onChange({ target: { id, value } })}
-    multiline={isMultiline}
-  />
-))`
+const InputContainer = styled(TextInput).attrs(({ isMultiline, type, id, onChange }) => ({
+  textAlignVertical: "top",
+  keyboardType: type === "email" ? "email-address" : undefined,
+  secureTextEntry: type === "password",
+  onChangeText: value => onChange({ target: { id, value } }),
+  multiline: isMultiline
+}))`
   padding: 16px;
   font-family: Montserrat-Regular;
   font-size: 16px;
@@ -24,16 +18,4 @@ const InputContainer = styled(({ forwardedRef, type, onChange, id, isMultiline, 
   color: ${({ theme }) => theme.fontColor};
 `;
 
-export default React.forwardRef(
-  // eslint-disable-line
-  (props, forwardedRef) => {
-    console.log(forwardedRef);
-    return (
-      <InputContainer
-        // eslint-disable-line
-        {...props}
-        forwardedRef={forwardedRef}
-      />
-    );
-  }
-);
+export default InputContainer;
