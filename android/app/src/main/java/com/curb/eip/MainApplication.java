e56b2eefbb22e6dd14e94d8f519f148446dd4683
MisterFail <jimmy.djabali@epitech.eu>
2019-11-27 18:53:44 +0100

    test

--- android/app/src/main/java/com/curb/eip/MainApplication.java
+++ android/app/src/main/java/com/curb/eip/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
