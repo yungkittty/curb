6690727e6643d80b55af5786dedc4d7bef54ea7e
Etsuro <adrien.bachelet@epitech.Eu>
2019-12-10 13:36:17 +0100

    RN datepicked corrected

--- src/components/event-create/components/event-datepicker/components/datepicker-data-day/datepicker-data-day.js
+++ src/components/event-create/components/event-datepicker/components/datepicker-data-day/datepicker-data-day.js
@@ -7,7 +7,8 @@ import DataContainer from "../data-container";
 const DatepickerDataDay = ({ date }) => (
   <DataContainer>
     <Icon size="extra-small" icon="calendar-day" color="#e0e0e0" style={{ marginRight: 8 }} />
-    <Text type="h4" weight={700}>{`${date.getDay()}  /  ${date.getMonth()}  /  ${date.getFullYear()}`}</Text>
+    <Text type="h4" weight={700}>{`${date.getDate()}  /  ${date.getMonth() +
+      1}  /  ${date.getFullYear()}`}</Text>
   </DataContainer>
 );
 
