1798ff68b4b499cb396e45eee01311e5a70411e8
Jimmy DJABALI <jimmy.djabali@epitech.eu>
2019-12-12 01:37:28 +0100

    Update button-container.ios.js

--- src/components/button-container/button-container.ios.js
+++ src/components/button-container/button-container.ios.js
@@ -8,7 +8,7 @@ const ButtonContainer = styled(({ onClick, ...others }) =>
   typeof onClick === "string" || onClick === "object" ? (
     <Link {...others} component={TouchableOpacity} to={onClick} />
   ) : (
-    <TouchableOpacity {...others} onPress={onClick} activeOpacity={1} />
+    <TouchableOpacity {...others} onPress={onClick} activeOpacity={0.2} />
   )
 )`
   display: flex;
